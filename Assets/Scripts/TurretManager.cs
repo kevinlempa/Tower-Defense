69f4c91fb7ed9d131b2e754e5b07471bcb20c3ef
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2021-02-24 15:56:03 +0100

    adds initial turret button

--- Assets/Scripts/TurretManager.cs
+++ Assets/Scripts/TurretManager.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TurretManager : MonoBehaviour {
     private Camera cam;
-    [SerializeField] private TurretsSO turretType;
+    [SerializeField] private TurretsSO activeTurretType;
     [SerializeField] private TurretTypes turretTypes;
 
     private void Start() {
@@ -14,9 +15,9 @@ public class TurretManager : MonoBehaviour {
     }
 
     private void Update() {
-        if (Input.GetMouseButtonDown(1)) {
-            var instance = Instantiate(turretType.prefab, GetMouseWorldPosition(), Quaternion.identity);
-            instance.GetComponent<Turret>().Setup(turretType);
+        if (Input.GetMouseButtonDown(1) && !EventSystem.current.IsPointerOverGameObject()) {
+            var instance = Instantiate(activeTurretType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+            instance.GetComponent<Turret>().Setup(activeTurretType);
             Debug.Log(GetMouseWorldPosition());
         }
 
@@ -28,14 +29,14 @@ public class TurretManager : MonoBehaviour {
     }
 
     private Vector3 GetMouseWorldPosition() {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hitInfo, 100))
             Debug.DrawRay(ray.origin, ray.direction * hitInfo.distance, Color.magenta, 5);
         return hitInfo.point;
     }
 
     private GameObject ClickedOnTurret() {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hitInfo, 100)) {
             Debug.DrawRay(ray.origin, ray.direction * hitInfo.distance, Color.magenta, 5);
             return hitInfo.collider.gameObject;
@@ -47,11 +48,13 @@ public class TurretManager : MonoBehaviour {
     private void Upgrade(GameObject turret) {
         if (turret == null) return;
         if (turret.gameObject.GetComponent<Turret>()) {
-            var upgrade = turretTypes.turretTypeList[turret.GetComponent<Turret>().turretData.index + 1];
-            var position = turret.transform.position;
-            Destroy(turret);
-            var instance = Instantiate(upgrade.prefab, position, Quaternion.identity);
-            instance.GetComponent<Turret>().Setup(turretType);
+            if (turret.GetComponent<Turret>().turretData.index+1 < turretTypes.turretTypeList.Count) {
+                var upgrade = turretTypes.turretTypeList[turret.GetComponent<Turret>().turretData.index + 1];
+                var position = turret.transform.position;
+                Destroy(turret);
+                var instance = Instantiate(upgrade.prefab, position, Quaternion.identity);
+                instance.GetComponent<Turret>().Setup(upgrade);
+            }
         }
     }
 }
