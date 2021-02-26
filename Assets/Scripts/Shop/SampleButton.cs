7a13df02350afafec11f417b1fd1e656c8e60883
Johan Hammarström <johan.hammarstrom@student.forsbergsskola.se>
2021-02-26 16:52:46 +0100

    ++ShopScene ++Shop scriptfolder ++Pooling script for objects ++ShopScrollList script ++Buttonprefab with script, name, price and icon.
    TODO: sprites/images for gems and gold, maybe chest with gold?

---
+++ Assets/Scripts/Shop/SampleButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SampleButton : MonoBehaviour
+{
+    public Button button;
+    public Text nameLabel;
+    public Text priceLabel;
+    public Image iconImage;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Setup()
+    {
+        
+    }
+    
+}
